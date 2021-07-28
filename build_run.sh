e0915e0ccf9dbb0c8e3589f8bab35caf0bc4e6ff
Michael Culshaw-Maurer <culshawmaurer@email.arizona.edu>
2021-07-27 17:32:04 -0700

    build_run now takes password as an argument

--- build_run.sh
+++ build_run.sh
@@ -4,4 +4,4 @@ docker build -t rocker-tidyverse-custom .
 
 open http://localhost:8787
 
-docker run -e PASSWORD=password_here -p 8787:8787 -v "$(pwd)"/output:/home/rstudio/output rocker-tidyverse-custom
+docker run -e PASSWORD="$1" -p 8787:8787 -v "$(pwd)"/output:/home/rstudio/output rocker-tidyverse-custom
